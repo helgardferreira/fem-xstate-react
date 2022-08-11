e9dbd3af0f8272a8dfcaff940511a707d88f03fd
Helgard Ferreira <hrfer3@gmail.com>
2022-08-11 12:34:09 +0200

    finish workshop

--- src/07/timerMachine.js
+++ src/07/timerMachine.js
@@ -1,75 +1,105 @@
-import { createMachine, assign } from 'xstate';
+import { createMachine, assign } from "xstate";
 
-const ticker = (context, event) => (callback) => {
-  // This is the callback service creator.
-  // Add the implementation details here.
-  // ...
+// Actions
+const resetTimer = assign({
+  duration: 5,
+  elapsed: 0,
+});
+const elapseTime = assign({
+  elapsed: (ctx) => ctx.elapsed + ctx.interval,
+});
+const addMinute = assign({
+  duration: (ctx) => ctx.duration + 60,
+});
+
+// Services
+// N.B. the context value that the service has access to is the context when
+// the service is invoked. If you need an updated context you will need
+// to use the send function to send an event to the service callback - remember
+// the ID!
+const ticker = (context, event) => (send) => {
+  const interval = setInterval(() => {
+    send({ type: "TICK" });
+  }, 1000 * context.interval);
+
+  return () => clearInterval(interval);
 };
 
 const timerExpired = (ctx) => ctx.elapsed >= ctx.duration;
 
-// https://xstate.js.org/viz/?gist=78fef4bd3ae520709ceaee62c0dd59cd
-export const timerMachine = createMachine({
-  id: 'timer',
-  initial: 'idle',
-  context: {
-    duration: 60,
-    elapsed: 0,
-    interval: 0.1,
-  },
-  states: {
-    idle: {
-      entry: assign({
-        duration: 60,
-        elapsed: 0,
-      }),
-      on: {
-        TOGGLE: 'running',
-        RESET: undefined,
-      },
-    },
-    running: {
-      // Invoke the callback service here.
-      // ...
-
-      initial: 'normal',
+export const timerMachine =
+  /** @xstate-layout N4IgpgJg5mDOIC5QBcCWBbMAnAdKiANmAMQAqA8gOKUAyAooqAA4D2sqaLAdoyAB6IAzAFYATDgCMADgDswgAyiZAFhkz5MwQBoQAT0RKAnDkFSAbGamCzwmVPlnRZgL7OdaTLnxFiAJToAynSkvKzsnDxI-AaixsKGgnJSooKiyhJmhjI6+ggShsIm5pmiwhISwlLCYq7uGNg4WACuXFyoXFA4XCxY6ACGBH6BwaFsHKjcvAIICjI4yqLS1jJOiaKlOUKGUjiK8vuG8sKCghLKwsq1IB4Nza3tnd29A8Sj4RORoNNmDjiayqc1MpDFk5JsEAsJDgxPsUokzIJDBZLm5rvVcHc2h0cCwAG7YG5kKi0BhRMLjSZRabJKFVC4yH6LQTKKSGcHKX5VVKssrqC6GK43DEtLFQMgASQAwgBpN4Uz7RBBmGRxFLKZWWI6I7J6RASNS7UT7CQicxSWSaQXoxoih5E6j0OURKZ6sQ4Q6OFmGNJlZlScHWQorY3KYFmfLHK2eG33DrEACCABFEwB9ACy4oAcgBVUik5hjZ1UxDKwqOcryLI-OyWcGHQ37I4m4HSeRSKMNJh9JqwSD2klOj4uvJuj1pVk+5v+3UQk1-I0HIH6rIouqeIZBEJkwtD4sQ0TgpziLUiceyQT7GSuVHdCBwXhCvCEMCDylfPUAkzydV0jL2GQSIeLJFBYFo8pk3odsKsaPD0-QEK+CrTAoyjzGcRzAss+zTrk5xQns+wAqcsSsquaLRpiDw4viWA3Ihw7JPIuwJJoRz2NI8TsvEOBcicSIIhclTCFBMaivRe4skx6QcvyWFtgGTi7PkIJ8tsJxnCJXY9pA4nvvuAbKiByqSRyShHFBumKtI4JmA2jb2Y2+rXs4QA */
+  createMachine(
+    {
+      context: { duration: 60, elapsed: 0, interval: 0.1 },
+      id: "timer",
+      initial: "idle",
       states: {
-        normal: {
-          always: {
-            target: 'overtime',
-            cond: timerExpired,
-          },
+        idle: {
+          entry: "resetTimer",
           on: {
-            RESET: undefined,
+            TOGGLE: {
+              target: "running",
+            },
+            RESET: {},
           },
         },
-        overtime: {
+        running: {
+          invoke: {
+            src: "ticker",
+          },
+          initial: "normal",
+          states: {
+            normal: {
+              always: {
+                cond: "timerExpired",
+                target: "overtime",
+              },
+              on: {
+                RESET: {},
+              },
+            },
+            overtime: {
+              on: {
+                TOGGLE: {},
+              },
+            },
+          },
           on: {
-            TOGGLE: undefined,
+            TICK: {
+              actions: "elapseTime",
+            },
+            TOGGLE: {
+              target: "paused",
+            },
+            ADD_MINUTE: {
+              actions: "addMinute",
+            },
+          },
+        },
+        paused: {
+          on: {
+            TOGGLE: {
+              target: "running",
+            },
           },
         },
       },
       on: {
-        TICK: {
-          actions: assign({
-            elapsed: (ctx) => ctx.elapsed + ctx.interval,
-          }),
-        },
-        TOGGLE: 'paused',
-        ADD_MINUTE: {
-          actions: assign({
-            duration: (ctx) => ctx.duration + 60,
-          }),
+        RESET: {
+          target: ".idle",
         },
       },
     },
-    paused: {
-      on: { TOGGLE: 'running' },
-    },
-  },
-  on: {
-    RESET: {
-      target: '.idle',
-    },
-  },
-});
+    {
+      actions: {
+        resetTimer,
+        elapseTime,
+        addMinute,
+      },
+      guards: { timerExpired },
+      services: {
+        ticker,
+      },
+    }
+  );
--- src/scratch/index.js
+++ src/scratch/index.js
@@ -1,9 +1,17 @@
-import React, { useEffect } from "react";
-import { createMachine, assign } from "xstate";
-import { useMachine } from "@xstate/react";
+import React from "react";
+import { createMachine, assign, send, spawn, sendParent } from "xstate";
+import { useMachine, useService } from "@xstate/react";
+
+const saveAlarm = async () => {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(100);
+    }, 2000);
+  });
+};
 
 const alarmMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QEMA2yBOBbAssgxgBYCWAdmAHRkEAuxAbmAMQAqA8gOIcAyAooqAAOAe1jE6w0gJAAPRAEYAbAFYKADjUAmNcoCcAdmUBmAAyaALJoA0IAJ6JNy-RWUm35o0fnLN+zfPMAX0CbNExcAhJyCkEwUggyKCYAZQBVAGF03mTk6RExCSkkWURzXSMKQ301eUcbewRTc3VPIwNFMt0yxWDQ9Gw8IjJKWPjE1k4efmL88WJJaTkEJVUNbT1DUwtrOwcTXRc3E3l5GqNHRRMgkJAwgcjhiloGSlJhbDQmGVgaZBpKZAAM3+GAAFK4TABKJh3CJDaLPRgUN4fVB5URzBbFJaGeQURzVXS1ZSucxmeqIUwVRQ0-SeS5qcyuXTKXq3fpwqIA-B0RgTLh8dEFeZFUBLbyrLQ6AzGMyWCkIXQmChHY5OZTyNxqEystlvCBwaSwwZcqikRFgIWY0UlBDy3YITS6TQqo5eVyaY6KeRs40PaKjBKkKBWwqLSk1CjnExGcxksxe+QKp0HVpGGkBfTlIy6vrhE2PC3I95YNChkXhhAdNQUcqbbwJpRJh1OmtKRT6MyajqxtS+jkFhE8l4UVDCYQAa1gHHHEHLWLFiDUugO-hMy7lihl+mTujbtK7Jh75j7Nz98O5vMtMwxYexiEUFlrRnrHsTyc0igo7cUakUOeUSwO1zdl839a8hFvCt72WNQFXkZwVyQ5CUKMYJgiAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QEMA2yBOBbAssgxgBYCWAdmAHRkEAuxAbmAMQAqA8gOIcAyAooqAAOAe1jE6w0gJAAPRAEYAbAFYKADjUAmNfIAsmxdoAMegDQgAnok3KA7BV0BOZ46NrnGgL6fzaTLgIScgpBMFIIMigmCElKMnphAGtKP2w8IjJKUPDIhHjhfGQJUgBtIwBdaRExYuk5BEVdRQpNWwBmXTbbTSNlZW01cysEPU11Non9RUUjJsU1ZW9fdDTAzJCwiNIosAwMYQwQ9BoAMwOsClSAjODsrag80gTC4rLKpBBq8WJJOsRG5qtDpdHp9AZDRBteRjNQTNqteZqRqOAxLEBXdJBLKbSKsTg8fgfL61D71Ay6ChGRxdZSKFFGFTzRQQhA9RwOFyOPQLIytPpojFrYK0BjMdhcPhVUTfX6kxC2WyqNSdWyOfSOdrKNS2FnuCicrkK2wzeQdbw+ECkYQQODSQU3OKkEWMKU1H5SOUIfQszRc-UuWG6ZVaumLC32rEbHLbV0yj2geptHQUeFGVMLI2wn0qFNwzTyXnTXS6eS2AUra6R51gWMkhOIdxtSm2NOmrXQ+aOFnyP1KabKLm9NyKRXl-yY9YYMAAKzA+BokFr7r+CEbzdbbXbBncLLaRnscNTwI1mjHqwdS9l9ZGg0sCjU5s8QA */
   createMachine(
     {
       context: { count: 0 },
@@ -20,17 +28,63 @@ const alarmMachine =
           },
         },
         pending: {
-          on: {
-            SUCCESS: {
-              target: "active",
+          invoke: {
+            id: "timeout",
+            // invoking service instead of actor
+            /* src: "saveAlarmService", */
+            src: (ctx, e) => (sendBack, receive) => {
+              console.log("invoking service");
+              // actor can choose whether to send or not based off of event
+              receive((event) => {
+                console.log(event);
+              });
+
+              const timeout = setTimeout(() => {
+                sendBack({
+                  type: "SUCCESS",
+                });
+              }, 2000);
+
+              return () => {
+                console.log("cleaning up!");
+                clearTimeout(timeout);
+              };
             },
+            // Using services example instead of actor
+            /* onDone: [
+              {
+                cond: (ctx, event) => {
+                  return event.data > 99;
+                },
+                target: "active",
+              },
+              {
+                target: "rejected",
+              },
+            ],
+            onError: [
+              {
+                target: "rejected",
+              },
+            ], */
+          },
+          on: {
+            SUCCESS: { target: "active" },
             TOGGLE: {
               target: "inactive",
+              // Sending a message to a callback actor
+              /* actions: send(
+                { type: "GOODBYE" },
+                {
+                  to: "timeout",
+                }
+              ), */
             },
           },
         },
         active: {
-          initial: "normal",
+          // Nested states example
+          /* initial: "normal",
           states: {
             normal: {
               after: {
@@ -40,13 +94,15 @@ const alarmMachine =
               },
             },
             looksGood: {},
-          },
+          }, */
+          entry: sendParent({ type: "ACTIVE" }),
           on: {
             TOGGLE: {
               target: "inactive",
             },
           },
         },
+        rejected: {},
       },
     },
     {
@@ -58,8 +114,41 @@ const alarmMachine =
       guards: {
         lessThanFive: (context, event) => context.count < 5,
       },
+      services: {
+        /* saveAlarmService: (ctx, event) => {
+          return saveAlarm();
+        }, */
+      },
     }
   );
+
+const alarmsMachine = createMachine({
+  id: "alarms",
+  initial: "active",
+  context: {
+    alarms: [],
+  },
+  states: {
+    active: {
+      on: {
+        ADD_ALARM: {
+          actions: assign({
+            alarms: (context, event) => {
+              const alarm = spawn(alarmMachine);
+
+              return context.alarms.concat(alarm);
+            },
+          }),
+        },
+        ACTIVE: {
+          actions: (context, event) => {
+            console.log("received event");
+          },
+        },
+      },
+    },
+  },
+});
 
 // inspect({
 //   iframe: false,
@@ -91,52 +180,46 @@ const alarmReducer = (state, event) => {
 };
 */
 
-export const ScratchApp = () => {
-  const [state, send] = useMachine(alarmMachine, {
-    actions: {
-      incrementCount: assign({
-        count: (context, event) => {
-          console.log(context.count);
-          return context.count + 1;
-        },
-      }),
-    },
-    devTools: true,
-  });
+const Alarm = ({ alarmRef }) => {
+  const [state, sendAlarm] = useService(alarmRef);
 
   const status = state.value; // "inactive" | "pending" | "active"
   const { count } = state.context;
 
-  useEffect(() => {
-    let timeout;
-    if (status === "pending") {
-      timeout = setTimeout(() => {
-        send({ type: "SUCCESS" });
-      }, 1000);
-    }
+  return (
+    <div className="alarm">
+      <div className="alarmTime">
+        {new Date().toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+        })}{" "}
+        ({count}) ({state.toStrings().slice(-1)[0]})
+      </div>
+      <div
+        className="alarmToggle"
+        data-active={status === "active" || undefined}
+        style={{
+          opacity: status === "pending" ? 0.5 : 1,
+        }}
+        onClick={() => sendAlarm({ type: "TOGGLE" })}
+      ></div>
+    </div>
+  );
+};
 
-    return () => clearTimeout(timeout);
-  }, [status, send]);
+export const ScratchApp = () => {
+  const [state, sendAlarms] = useMachine(alarmsMachine, { devTools: true });
+
+  const { alarms } = state.context;
 
   return (
     <div className="scratch">
-      <div className="alarm">
-        <div className="alarmTime">
-          {new Date().toLocaleTimeString("en-US", {
-            hour: "2-digit",
-            minute: "2-digit",
-          })}{" "}
-          ({count}) ({state.toStrings().slice(-1)[0]})
-        </div>
-        <div
-          className="alarmToggle"
-          data-active={status === "active" || undefined}
-          style={{
-            opacity: status === "pending" ? 0.5 : 1,
-          }}
-          onClick={() => send({ type: "TOGGLE" })}
-        ></div>
-      </div>
+      <button onClick={() => sendAlarms({ type: "ADD_ALARM" })}>
+        Add Alarm
+      </button>
+      {alarms.map((alarm) => {
+        return <Alarm key={alarm.id} alarmRef={alarm} />;
+      })}
     </div>
   );
 };
